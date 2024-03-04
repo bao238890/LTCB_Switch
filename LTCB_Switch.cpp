6b00f5cd07e5664ee359fb0486b37c828488ec25
ADMIN <ADMIN@LAB1-MAY02>
2024-03-04 08:08:17 +0700

    Add project files.

---
+++ LTCB_Switch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int day = 6;
+	switch (day) {
+	case 1:
+		cout << "Thu hai";
+		break;
+	case 2:
+		cout << "Thu ba";
+		break;
+	case 3:
+		cout << "Thu tu";
+		break;
+	case 4:
+		cout << "Thu nam";
+		break;
+	case 5:
+		cout << "Thu sau";
+		break;
+	case 6:
+		cout << "Thu bay";
+		break;
+	case 7:
+		cout << "Chu nhat";
+		break;
+	}
+}
